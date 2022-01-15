09e1f32cf760d1571b37ca2f395889eb28411b9a
Wen Xiao <wennstyle@gmail.com>
2022-01-14 22:39:27 -0500

    commit tree 52098b2eaa0726802f5c19dc2b1ebf9b1dc5936e

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const h1Style = {
+  color: 'firebrick'
+}
+
 function Home() {
   // update the JSX being returned!
   return (
   <div id="home">
-  <h1 style="color:firebrick;"> {name} is a Web Developer from {city}
+  <h1 style={h1Style}> {name} is a Web Developer from {city}
   </h1>
   </div>);
 }
 
 export default Home;
+
+
+//Alternatively inline styling can be written as
+//style={{color: 'firebrick'}}
